3b691b4906d2f9503e62100db8abd64d552590d4
thuydotp <phuonthuydt0311@gmail.com>
2018-07-15 23:16:32 +0700

    Create project

---
+++ PersonalBlog/Data/PersonalBlogContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Models;
+
+namespace PersonalBlog.Persistence
+{
+    public class PersonalBlogContext : DbContext
+    {
+        public PersonalBlogContext (DbContextOptions<PersonalBlogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PersonalBlog.Models.CategoryModel> CategoryModel { get; set; }
+
+        public DbSet<PersonalBlog.Models.PostModel> PostModel { get; set; }
+    }
+}
---
+++ PersonalBlog/Models/PostModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Models
+{
+    public class PostModel
+    {
+        public Guid Id { get; set; }
+        public string Slug { get; set; }
+        public string ShortDescription { get; set; }
+        public string MainContent { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime UpdatedDate { get; set; }
+    }
+}
